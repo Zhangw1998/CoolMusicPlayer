ec862ce3dc30a888af97af0b7fccc1c12553b66c
zhangwww <zhangwww9799@gmail.com>
2022-02-23 21:58:18 +0800

    first commit

---
+++ app/src/main/java/com/cool/musicplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cool.musicplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
